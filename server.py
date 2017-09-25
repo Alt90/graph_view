e2be0458936f906f377966a37c8768ae9e1f9ce4
Alt90 <alt90@rambler.ru>
2017-09-25 22:00:14 +0300

    Добавил ручку стартовой страницы и обработку 404

--- server.py
+++ server.py
@@ -4,5 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return 'Ок!', 200
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Вы потерялись:( Но я вам   <a href="/">помогу</a>!', 404
+
+
 if __name__ == '__main__':
     app.run()
